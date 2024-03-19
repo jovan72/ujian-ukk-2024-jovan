250d617db6b1ab812c65c1ff68bba7f669be6c95
jovan72 <163383908+jovan72@users.noreply.github.com>
2024-03-19 12:46:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>aplikasikalkulator</title>
+     <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+ <div class="kotak">
+     <table border="1" cellspacing="0">
+		<tr>
+			<td colspan="4" id="inputan">0</td>
+		</tr>
+		<tr>
+      		<td colspan="2"><button onclick="tombol(this)">AC</button></td>
+      		<td><button onclick="tombol(this)">Del</button></td>
+      		<td><button onclick="tombol(this)">/</button></td>
+		</tr>
+		<tr>
+      		<td><button onclick="tombol(this)">7</button></td>
+     	 	<td><button onclick="tombol(this)">8</button></td>
+      		<td><button onclick="tombol(this)">9</button></td>
+      		<td><button onclick="tombol(this)">*</button></td>
+		</tr>
+		<tr>
+      		<td><button onclick="tombol(this)">4</button></td>
+      		<td><button onclick="tombol(this)">5</button></td>
+      		<td><button onclick="tombol(this)">6</button></td>
+      		<td><button onclick="tombol(this)">+</button></td>
+		</tr>
+		<tr>
+      		<td><button onclick="tombol(this)">1</button></td>
+      		<td><button onclick="tombol(this)">2</button></td>
+      		<td><button onclick="tombol(this)">3</button></td>
+      		<td><button onclick="tombol(this)">-</button></td>
+		</tr>
+		<tr>
+      		<td colspan="2"><button onclick="tombol(this)">0</button></td>
+      		<td><button onclick="tombol(this)">.</button></td>
+      		<td><button onclick="tombol(this)">=</button></td>
+     	</tr>
+   </table>
+</div>
+<script type="text/javascript" src="./js/script.js"></script>
+</body>
+</html> 
